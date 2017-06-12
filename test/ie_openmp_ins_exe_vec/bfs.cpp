26cb579f6d68ac2a9934a9dd04aa9455ae7dbc4e
chuqixiaozhu <chuqixiaozhu@gmail.com>
2017-06-12 17:27:11 -0400

    right openmp

---
+++ test/ie_openmp_ins_exe_vec/bfs.cpp
@@ -0,0 +1,314 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <immintrin.h>
+
+#define NO_P_INT 16 // Number of packed integers in one __m512i
+#define ALIGNED_BYTES 64
+//#define SIZE_INT 4
+#define SIZE_INT sizeof(int)
+#define OPEN
+
+//#define BUFFER_SIZE_MAX 134217728 // 2^27
+//#define BUFFER_SIZE_MAX 4096 //
+unsigned BUFFER_SIZE_MAX;
+
+FILE *fp;
+
+//Structure to hold a node information
+struct Node
+{
+	int starting;
+	int no_of_edges;
+};
+
+void BFSGraph(int argc, char** argv);
+
+//void Usage(int argc, char**argv){
+void Usage( char**argv){
+
+fprintf(stderr,"Usage: %s <num_threads> <input_file>\n", argv[0]);
+
+}
+////////////////////////////////////////////////////////////////////////////////
+// Main Program
+////////////////////////////////////////////////////////////////////////////////
+int main( int argc, char** argv) 
+{
+	BFSGraph( argc, argv);
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+//Apply BFS on a Graph using CUDA
+////////////////////////////////////////////////////////////////////////////////
+void BFSGraph( int argc, char** argv) 
+{
+	unsigned int no_of_nodes = 0;
+	int edge_list_size = 0;
+	char *input_f;
+	int	 num_omp_threads;
+	
+	if(argc!=4){
+	//Usage(argc, argv);
+	//Usage( argv);
+	//exit(0);
+	//num_omp_threads = 1;
+	num_omp_threads = 4;
+	static char add[] = "/home/zpeng/benchmarks/rodinia_3.1/data/bfs/graph4096.txt";
+	input_f = add;
+	//BUFFER_SIZE_MAX = 4096;
+	BUFFER_SIZE_MAX = 64;
+	} else {
+	num_omp_threads = atoi(argv[1]);
+	input_f = argv[2];
+	BUFFER_SIZE_MAX = atoi(argv[3]);
+	}
+	
+	//printf("Reading File\n");
+	//Read in Graph from a file
+	fp = fopen(input_f,"r");
+	if(!fp)
+	{
+		printf("Error Reading graph file\n");
+		return;
+	}
+
+	int source = 0;
+
+	fscanf(fp,"%ud",&no_of_nodes);
+   
+	// allocate host memory
+	Node* h_graph_nodes = (Node*) malloc(sizeof(Node)*no_of_nodes);
+	int *h_graph_mask = (int*) _mm_malloc(SIZE_INT*no_of_nodes, ALIGNED_BYTES);
+	int *h_updating_graph_mask = (int*) _mm_malloc(SIZE_INT*no_of_nodes, ALIGNED_BYTES);
+	int *h_graph_visited = (int*) _mm_malloc(SIZE_INT*no_of_nodes, ALIGNED_BYTES);
+
+	int start, edgeno;   
+	// initalize the memory
+	for( unsigned int i = 0; i < no_of_nodes; i++) 
+	{
+		fscanf(fp,"%d %d",&start,&edgeno);
+		h_graph_nodes[i].starting = start;
+		h_graph_nodes[i].no_of_edges = edgeno;
+		h_graph_mask[i]=0;
+		h_updating_graph_mask[i]=0;
+		h_graph_visited[i]=0;
+	}
+
+	//read the source node from the file
+	fscanf(fp,"%d",&source);
+	// source=0; //tesing code line
+
+	//set the source node as true in the mask
+	h_graph_mask[source]=1;
+	h_graph_visited[source]=1;
+
+	fscanf(fp,"%d",&edge_list_size);
+
+	int id,cost;
+	int* h_graph_edges = (int*) _mm_malloc(SIZE_INT*edge_list_size, ALIGNED_BYTES);
+
+	for(unsigned int i=0; i < edge_list_size ; i++)
+	{
+		fscanf(fp,"%d",&id);
+		fscanf(fp,"%d",&cost);
+		h_graph_edges[i] = id;
+	}
+
+	if(fp)
+		fclose(fp);    
+
+
+	// allocate mem for the result on host side
+	int* h_cost = (int*) _mm_malloc( SIZE_INT*no_of_nodes, ALIGNED_BYTES);
+	for(unsigned int i=0;i<no_of_nodes;i++)
+		h_cost[i]=-1;
+	h_cost[source]=0;
+	
+	//printf("Start traversing the tree\n");
+	
+	//int k=0;
+	const __m512i one_v = _mm512_set1_epi32(1);
+	const __m512i minusone_v = _mm512_set1_epi32(-1);
+	const __m512i zero_v = _mm512_set1_epi32(0);
+
+	int **id_buffer = (int **) _mm_malloc(sizeof(int *) * num_omp_threads, ALIGNED_BYTES);
+	int **cost_buffer = (int **) _mm_malloc(sizeof(int *) * num_omp_threads, ALIGNED_BYTES);
+	for (unsigned i = 0; i < num_omp_threads; ++i) {
+		id_buffer[i] = (int *) _mm_malloc(SIZE_INT * BUFFER_SIZE_MAX, ALIGNED_BYTES);
+		cost_buffer[i] = (int *) _mm_malloc(SIZE_INT * BUFFER_SIZE_MAX, ALIGNED_BYTES);
+	}
+	unsigned top = 0;
+	int thrd;
+#ifdef OPEN
+        double start_time = omp_get_wtime();
+#endif
+	int stop;
+	do
+	{
+		//if no thread changes this value then the loop stops
+		stop = 1;
+
+#ifdef OPEN
+		omp_set_num_threads(num_omp_threads);
+#pragma omp parallel private(top, thrd)
+	{
+		top = 0;
+		thrd = omp_get_thread_num();
+#pragma omp for schedule(dynamic)
+#endif 
+		for(unsigned int tid = 0; tid < no_of_nodes; tid++ )
+		{
+			if (h_graph_mask[tid] == 1) {
+				h_graph_mask[tid]=0;
+				int next_starting = h_graph_nodes[tid].starting + h_graph_nodes[tid].no_of_edges;
+
+				for (int i = h_graph_nodes[tid].starting; \
+						 i < next_starting; \
+						 i += NO_P_INT) {
+					/* Check buffer's size */
+					if (top + NO_P_INT > BUFFER_SIZE_MAX) {
+						/* If Buffer would be full, then operate */
+#ifdef OPEN
+//#pragma omp parallel for
+//#pragma omp for
+#endif
+						for (unsigned int k = 0; \
+								k < top; \
+								k += NO_P_INT) {
+							if (k + NO_P_INT <= top) {
+								/* Vectoried */
+								/* Update those flags */
+								__m512i id_v = _mm512_load_epi32(id_buffer[thrd] + k);
+								__m512i visited_v = _mm512_i32gather_epi32(id_v, h_graph_visited, SIZE_INT);
+								__mmask16 novisited_mask = _mm512_cmpeq_epi32_mask(visited_v, zero_v);
+								_mm512_mask_i32scatter_epi32(h_updating_graph_mask, novisited_mask, id_v, one_v, SIZE_INT);
+								_mm512_mask_i32scatter_epi32(h_graph_visited, novisited_mask, id_v, one_v, SIZE_INT);
+								//stop = 0;
+
+								/* Update the h_cost */
+								__m512i cost_source_v = _mm512_load_epi32(cost_buffer[thrd] + k);
+								__m512i cost_v = _mm512_add_epi32(cost_source_v, one_v);
+								_mm512_mask_i32scatter_epi32(h_cost, novisited_mask, id_v, cost_v, SIZE_INT);
+							} else {
+								/* Serialized */
+								for (unsigned int j = k; \
+										j < top; \
+										j++) {
+									int id = id_buffer[thrd][j];
+									if (!h_graph_visited[id]) {
+										h_updating_graph_mask[id] = 1;
+										h_graph_visited[id] = 1;
+										//stop = 0;
+										h_cost[id] = cost_buffer[thrd][j] + 1;
+									}
+								}
+							}
+						}
+
+						top = 0;
+					}
+					/* Load to buffer */
+					__m512i id_v = _mm512_loadu_si512(h_graph_edges + i);
+					_mm512_store_epi32(id_buffer[thrd] + top, id_v);
+					__m512i cost_source_v = _mm512_set1_epi32(h_cost[tid]);
+					_mm512_store_epi32(cost_buffer[thrd] + top, cost_source_v);
+					if (i + NO_P_INT <= next_starting) {
+						top += NO_P_INT;
+					} else {
+						top += next_starting - i;
+					}
+				}
+			}
+		}
+#ifdef OPEN
+//#pragma omp parallel for
+#endif
+		for (unsigned int i = 0; \
+			 i < top; \
+			 i += NO_P_INT) {
+			if (i + NO_P_INT <= top) {
+				/* Vectoried */
+				/* Update those flags */
+				__m512i id_v = _mm512_load_epi32(id_buffer[thrd] + i);
+				__m512i visited_v = _mm512_i32gather_epi32(id_v, h_graph_visited, SIZE_INT);
+				__mmask16 novisited_mask = _mm512_cmpeq_epi32_mask(visited_v, zero_v);
+				_mm512_mask_i32scatter_epi32(h_updating_graph_mask, novisited_mask, id_v, one_v, SIZE_INT);
+				_mm512_mask_i32scatter_epi32(h_graph_visited, novisited_mask, id_v, one_v, SIZE_INT);
+				//stop = 0;
+
+				/* Update the h_cost */
+				__m512i cost_source_v = _mm512_load_epi32(cost_buffer[thrd] + i);
+				__m512i cost_v = _mm512_add_epi32(cost_source_v, one_v);
+				_mm512_mask_i32scatter_epi32(h_cost, novisited_mask, id_v, cost_v, SIZE_INT);
+			} else {
+				/* Serialized */
+				for (unsigned int j = i; \
+						j < top; \
+						j++) {
+					int id = id_buffer[thrd][j];
+					if (!h_graph_visited[id]) {
+						h_updating_graph_mask[id] = 1;
+						h_graph_visited[id] = 1;
+						//stop = 0;
+						h_cost[id] = cost_buffer[thrd][j] + 1;
+					}
+				}
+			}
+		}
+#ifdef OPEN
+//#pragma omp parallel for
+//#pragma omp for schedule(dynamic)
+//#pragma omp for
+#endif
+		for (unsigned i = 0; i < no_of_nodes; i += NO_P_INT) {
+			__m512i id_v = _mm512_load_epi32(h_updating_graph_mask + i);
+			__mmask16 updated_v = _mm512_cmpeq_epi32_mask(id_v, one_v);
+			if (*((short *)(&updated_v)) != 0) {
+				if (stop) {
+					stop = 0;
+				}
+				_mm512_store_epi32(h_graph_mask + i, id_v);
+				_mm512_store_epi32(h_updating_graph_mask + i, zero_v);
+			}
+		}
+
+#ifdef OPEN
+	}
+#endif
+	}
+	while(!stop);
+#ifdef OPEN
+        double end_time = omp_get_wtime();
+		printf("%d %lf\n", num_omp_threads, (end_time - start_time));
+		//printf("%u %lf\n", BUFFER_SIZE_MAX, (end_time - start_time));
+#endif
+	//Store the result into a file
+	FILE *fpo = fopen("path.txt","w");
+	for(unsigned int i=0;i<no_of_nodes;i++)
+		fprintf(fpo,"%d) cost:%d\n",i,h_cost[i]);
+	fclose(fpo);
+	//printf("Result stored in result.txt\n");
+
+	// cleanup memory
+	free( h_graph_nodes);
+	_mm_free( h_graph_edges);
+	_mm_free( h_graph_mask);
+	_mm_free( h_updating_graph_mask);
+	_mm_free( h_graph_visited);
+	_mm_free( h_cost);
+	//_mm_free( id_buffer);
+	//_mm_free( tid_buffer);
+	//_mm_free( cost_buffer);
+	for (unsigned i = 0; i < num_omp_threads; ++i) {
+		_mm_free(id_buffer[i]);
+		_mm_free(cost_buffer[i]);
+	}
+	_mm_free(id_buffer);
+	_mm_free(cost_buffer);
+}
+
---
+++ test/trash/deprecated_ie_openmp_ins_exe_vec/run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+# Determine the data file
+case $1 in
+4096)
+	data_file="graph4096"
+	;;
+128M)
+	data_file="graph128M"
+	;;
+*)
+	data_file="graph16M"
+	;;
+esac
+version="ins-exe-vec"
+bin_addr="/home/zpeng/benchmarks/test/ie_openmp_ins_exe_vec"
+data_addr="/home/zpeng/benchmarks/rodinia_3.1/data/bfs"
+no_core=64
+#power_max=16
+result_file="result_${version}_${data_file}_$(date +%Y%m%d-%H%M%S).txt"
+
+touch $result_file
+echo "Threads I/E-Vec" >> $result_file
+#echo "Buffer_Size Time" >> $result_file
+tno=1
+#power=4
+#size=$((2 ** $power))
+while [ $tno -le $no_core ]
+#while [ $power -le $power_max ]
+do
+#${bin_addr}/bfs ${tno} ${data_addr}/${data_file}.txt 4096 >> $result_file
+${bin_addr}/bfs ${tno} ${data_addr}/${data_file}.txt 64 >> $result_file
+#${bin_addr}/bfs 1 ${data_addr}/${data_file}.txt ${size} >> $result_file
+	echo -n .
+	tno=$((tno*2))
+#power=$((power + 1))
+#size=$((2 ** $power))
+done
+echo done.
