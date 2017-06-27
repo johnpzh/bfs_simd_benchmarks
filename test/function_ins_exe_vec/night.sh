2d0d60ab77ed65d08d83ed9d680836de2492e1b7
chuqixiaozhu <chuqixiaozhu@gmail.com>
2017-06-27 10:10:31 -0400

    result

--- test/function_ins_exe_vec/night.sh
+++ test/function_ins_exe_vec/night.sh
@@ -1,16 +1,6 @@
 #!/usr/bin/bash
 
-cd amplxe-report/
-./ddr_amplxe.sh 4 64 l
-./ddr_amplxe.sh 4 64 o
-./ddr_amplxe.sh 4 256 l
-./ddr_amplxe.sh 4 256 o
-./mcdr_amplxe.sh 4 64 l
-./mcdr_amplxe.sh 4 64 o
-./mcdr_amplxe.sh 4 256 l
-./mcdr_amplxe.sh 4 256 o
-
-cd ../hatch/
+cd hatch/
 for ((i = 0; i < 4; ++i))
 do
 	./run.sh 16M
@@ -19,14 +9,24 @@ done
 for ((i = 0; i < 4; ++i))
 do
 	numactl -m 0 ./run.sh 16M
-doen
+done
 
 for ((i = 0; i < 4; ++i))
 do
 	numactl -m 1 ./run.sh 16M
-doen
+done
 
 for ((i = 0; i < 2; ++i))
 do
 	./run.sh 128M
 done
+
+cd ../amplxe-report/
+./ddr_amplxe.sh 4 64 l
+./ddr_amplxe.sh 4 64 o
+./ddr_amplxe.sh 4 256 l
+./ddr_amplxe.sh 4 256 o
+./mcdr_amplxe.sh 4 64 l
+./mcdr_amplxe.sh 4 64 o
+./mcdr_amplxe.sh 4 256 l
+./mcdr_amplxe.sh 4 256 o
